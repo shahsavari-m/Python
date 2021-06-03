abde8680b4858ffc39ee3d742fbd0ed54423a95f
shahsavari-m <84908564+shahsavari-m@users.noreply.github.com>
2021-06-03 16:22:53 +0430

    Create dastoor.py

---
+++ dastoor.py
@@ -0,0 +1,11 @@
+input_tool = float(input("enter tool\n"))
+input_arz = float(input("enter arz\n"))
+input_dastoor = input("enter dastoor")
+def mostatil_1 (tool, arz , dastoor)
+      if dastoor == mohit :
+           mohit = tool*2 + arz*2
+           print("\nmohit:\n" , mohit)
+       else if dastoor == masahat :
+           masahat = tool * arz
+           print("masahat" , masahat)
+mostatil_1 (input_tool, input_arz , input_dastoor)
