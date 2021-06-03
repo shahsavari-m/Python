33d484fa88c6ab269980f86907b0187db14a0844
shahsavari-m <84908564+shahsavari-m@users.noreply.github.com>
2021-06-03 15:52:23 +0430

    Create Mohit.py

---
+++ Mohit.py
@@ -0,0 +1,6 @@
+def calculate_mohit(tool , arz):
+     mohit = tool*2 + arz*2
+     print("mohit:\n" , mohit)
+input_tool = float(input("enter tool\n\n"))
+input_arz = float(input("enter arz\n\n"))
+calculate_mohit (input_tool , input_arz)
