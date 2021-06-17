9b8f1ca47a845807110ef152c4475720694968a3
shahsavari-m <84908564+shahsavari-m@users.noreply.github.com>
2021-06-17 17:54:43 +0430

    Create list-55.py

---
+++ list-55.py
@@ -0,0 +1,5 @@
+last_names = ['sedaght', 'zabihi', 'ahmadi' , 'geraeli' , 'shahsavari', 'norozi','rasooli', ' bagheri' , 'keshvarz' ]
+
+for name in last_names:
+    for charac in name :
+        print(charac)
