65b4f540f5d54728060c580db61e847372194d4f
shahsavari-m <84908564+shahsavari-m@users.noreply.github.com>
2021-06-17 16:47:51 +0430

    Create list-agh.py

---
+++ list-agh.py
@@ -0,0 +1,8 @@
+gender_m = ['mahamad', 'amin', 'farokh', 'reza', 'meysam']
+gender_f =['paniz', 'zeinab', 'mehrarna']
+
+for first_name in gender_m :
+    print(f'salam agha {first_name}')
+
+for first_name2 in gender_f:
+        print (f'salam khanoom {first_name2})
