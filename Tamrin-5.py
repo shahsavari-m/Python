b127243958e44afc06c9fe3d630265c956d241ae
shahsavari-m <84908564+shahsavari-m@users.noreply.github.com>
2021-06-23 16:59:31 +0430

    Create Tamrin-5.py

---
+++ Tamrin-5.py
@@ -0,0 +1,59 @@
+
+# Tmrin 1 - Chasbandan ESM , FAMIL bedoon zip
+
+
+# first_names = ['paniz', 'zeinab', 'mahnaz', 'mehrarna' , 'mohammad', 'amin', 'farokh', 'reza', 'meysam']
+# last_names = ['sedaght', 'zabihi', 'ahmadi' , 'geraeli' , 'shahsavari', 'norozi','rasooli', 'keshvarz', 'bagheri' ]
+#
+# for start in range(len (first_names)) :
+#     print(f' {first_names[start]} {last_names[start]}')
+#     start +=1
+
+# ################################################################################################
+
+# Tamrin 2 - az 10 ta 1 chap kond ba for
+
+# for adad in range(10,0,-1):
+#     print(adad, end= " \t")
+
+# ################################################################################################
+
+# Tramin 3 - jam adad list ba FOR
+# my_numbers = [1, 5, 10, 152]
+#
+# total= 0
+# for adad in my_numbers:
+#
+#     total += adad
+#
+# print(total)
+
+
+# ################################################################################################
+
+# Tramin 3 - jam adad list ba Tabe bazgashti
+# total =0
+# my_numbers = [1, 5, 10, 152]
+#
+# def jam_list(adad):
+# my_numbers[]
+#     total += adad
+#     return total
+#
+# print()
+
+# ################################################################################################
+# Tamrin Ezafe - Print index list
+# first_names = ['paniz', 'zeinab', 'mahnaz', 'mehrarna' , 'mohammad', 'amin', 'farokh', 'reza', 'meysam']
+# input_name = input("esm vared konind\n")
+# index_1 = 0
+#
+# for name in first_names :
+#     index_1 +=1
+#     if input_name == name :
+#             print (f' shomare andis {name} {index_1-1} mibashad')
+#
+#     #elif input_name != name :
+#
+#     #    print("esteb")
+#     #break
