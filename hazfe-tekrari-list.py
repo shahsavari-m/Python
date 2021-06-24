5b499c9d69d63ae69404583dbcb4e81fd29cfe42
shahsavari-m <84908564+shahsavari-m@users.noreply.github.com>
2021-06-24 09:38:02 +0430

    Create hazfe-tekrari-list.py

---
+++ hazfe-tekrari-list.py
@@ -0,0 +1,7 @@
+my_list = [1, 2, 3, 3, 4, 5, 6, 7, 7, 7, 8]
+index_1 = 0
+#def hazf_tekrari( adad)
+for adad in my_list:
+        index_1 +=1
+        if adad != my_list[index_1]:
+            print (adad)
