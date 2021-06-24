a9f29afb2c2270431899a534c84bc9fa572c29ea
shahsavari-m <84908564+shahsavari-m@users.noreply.github.com>
2021-06-24 14:51:47 +0430

    Create bedoon-zip2.py

---
+++ bedoon-zip2.py
@@ -0,0 +1,6 @@
+first_names = ['paniz', 'zeinab', 'mahnaz', 'mehrarna' , 'mohammad', 'amin', 'farokh', 'reza', 'meysam']
+last_names = ['sedaght', 'zabihi', 'ahmadi' , 'geraeli' , 'shahsavari', 'norozi','rasooli', 'keshvarz', 'bagheri' ]
+
+for start in range(len (first_names)) :
+     print(f' {first_names[start]} {last_names[start]}')
+     start +=1
