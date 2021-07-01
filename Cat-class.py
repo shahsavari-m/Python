b331d4c6571175c2d1bda17c3cbaf4855121f0d3
shahsavari-m <84908564+shahsavari-m@users.noreply.github.com>
2021-07-01 16:25:58 +0430

    Create Cat-class.py

---
+++ Cat-class.py
@@ -0,0 +1,10 @@
+
+class Cat:
+    def __init__ (self, name, age, color, country):
+        self.name = name
+        self.age = age
+        self.color = color
+        self.country = country
+
+Koko = Cat("Koko", 3 , "gray", "Iran")
+print(Koko.name, Koko.color, Koko.country)
